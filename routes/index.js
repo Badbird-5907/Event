ad7bbb53c9edd188405d51cb540a33c1dece2dfd
Badbird-5907 <evan.lele.yu2@gmail.com>
2021-10-30 18:04:29 -0400

    aaa

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/deez_nuts',function (req,res){
+  res.render('puzzletwo')
+});
+router.get('/finishedlol',function (req,res){
+  res.render('finish')
+});
+
+module.exports = router;
